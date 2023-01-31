7e55b2d7100cab197c305a2874799c459757c1a7
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-01-31 16:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function countWord() {
+	let text = document.getElementById('evaluatedText').value;
+	let show = document.getElementById('wordCount');
+	var count = 0;
+	var split =text.split(' ');
+
+	for (var i = 0; i < split.length; i++) {
+		if (split[i] != "") {
+			count += 1;
+		}
+	}
+	show.innerHTML = count;
+}
